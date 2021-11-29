65ae2608ecff22a54eb8b649e69be7fd624aaee5
eatae <al-loco@mail.ru>
2021-11-29 22:12:01 +0300

    changes after fix conflicts

--- tests/goodAutoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(
-    function($class) {
-        $fileName = __DIR__ . '/' . str_replace('\\', '/', $class) . php;
-
-        if (file_exists($fileNAme))
-            require $fileName;
-    }
-);
